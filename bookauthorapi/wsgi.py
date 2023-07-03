1c3258a3a7f6274873f36d79142d0cc70a6fad21
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-07-03 21:48:40 +0300

    first commit

--- bookauthorapi/wsgi.py
+++ bookauthorapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookauthorapitest2 project.
+WSGI config for bookauthorapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookauthorapitest2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookauthorapi.settings')
 
 application = get_wsgi_application()
